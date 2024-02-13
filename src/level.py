cab0d1ae753dc7132d5aad98b4f26cdbb06d7440
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-02-13 12:56:44 -0300

    feat: moving objects

--- src/level.py
+++ src/level.py
@@ -1,5 +1,5 @@
 from settings import *
-from sprites import Sprite, AnimatedSprite, MovingSprite
+from sprites import Sprite, AnimatedSprite, Spike, MovingSprite
 from player import Player
 from groups import AllSprites
 
@@ -84,7 +84,30 @@ class Level:
 
         # moving objects
         for obj in tmx_map.get_layer_by_name('Moving Objects'):
-            if obj.name == 'helicopter':
+            if obj.name == 'spike':
+                Spike(
+                    pos = (obj.x + obj.width, obj.y + obj.height),
+                    surf = level_frames['spike'],
+                    radius = obj.properties['radius'],
+                    speed = obj.properties['speed'],
+                    start_angle = obj.properties['start_angle'],
+                    end_angle = obj.properties['end_angle'],
+                    groups = (self.all_sprites, self.damage_sprites)
+                )
+                for radius in range(0, obj.properties['radius'], 20):
+                    Spike(
+                        pos = (obj.x + obj.width, obj.y + obj.height),
+                        surf = level_frames['spike_chain'],
+                        radius = radius,
+                        speed = obj.properties['speed'],
+                        start_angle = obj.properties['start_angle'],
+                        end_angle = obj.properties['end_angle'],
+                        groups = self.all_sprites,
+                        z = Z_LAYERS['bg details']
+                    )
+            else:
+                frames = level_frames[obj.name]
+                groups = (self.all_sprites, self.sem_collision_sprites) if obj.properties['platform'] else (self.all_sprites, self.damage_sprites)
                 if obj.width > obj.height:  # horizontal
                     move_dir = 'x'
                     start_pos = (obj.x, obj.y + obj.height / 2)
@@ -94,8 +117,19 @@ class Level:
                     start_pos = (obj.x + obj.width / 2, obj.y)
                     end_pos = (obj.x + obj.width / 2, obj.y + obj.height)
                 speed = obj.properties['speed']
-                MovingSprite((self.all_sprites, self.sem_collision_sprites),
-                             start_pos, end_pos, move_dir, speed)
+                MovingSprite(frames, groups, start_pos, end_pos, move_dir, speed, obj.properties['flip'])
+
+                if obj.name == 'saw':
+                    if move_dir == 'x':
+                        y = start_pos[1] - level_frames['saw_chain'].get_height() / 2
+                        left, right = int(start_pos[0]), int(end_pos[0])
+                        for x in range(left, right, 20):
+                            Sprite((x, y), level_frames['saw_chain'], self.all_sprites, Z_LAYERS['bg details'])
+                    else:
+                        x = start_pos[0] - level_frames['saw_chain'].get_width() / 2
+                        top, bottom = int(start_pos[1]), int(end_pos[1])
+                        for y in range(top, bottom, 20):
+                            Sprite((x, y), level_frames['saw_chain'], self.all_sprites, Z_LAYERS['bg details'])
 
     def run(self, dt):
         self.all_sprites.update(dt)
--- src/sprites.py
+++ src/sprites.py
@@ -1,4 +1,5 @@
 from settings import *
+from math import sin, cos, radians
 
 
 class Sprite(pygame.sprite.Sprite):
@@ -26,12 +27,10 @@ class AnimatedSprite(Sprite):
         self.animate(dt)
 
 
-class MovingSprite(Sprite):
+class MovingSprite(AnimatedSprite):
 
-    def __init__(self, groups, start_pos, end_pos, move_dir, speed):
-        surf = pygame.Surface((200, 50))
-        super().__init__(start_pos, surf, groups)
-        self.image.fill('white')
+    def __init__(self, frames, groups, start_pos, end_pos, move_dir, speed, flip=False):
+        super().__init__(start_pos, frames, groups)
         if move_dir == 'x':
             self.rect.midleft = start_pos
         else:
@@ -46,6 +45,10 @@ class MovingSprite(Sprite):
         self.direction = vector(1, 0) if move_dir == 'x' else vector(0, 1)
         self.move_dir = move_dir
 
+        # flip
+        self.flip = flip
+        self.reverse = {'x': False, 'y': False}
+
     def check_border(self):
         if self.move_dir == 'x':
             if self.rect.right >= self.end_pos[0] and self.direction.x == 1:
@@ -54,6 +57,7 @@ class MovingSprite(Sprite):
             if self.rect.left <= self.start_pos[0] and self.direction.x == -1:
                 self.direction.x = 1
                 self.rect.left = self.start_pos[0]
+            self.reverse['x'] = True if self.direction.x < 0 else False
         else:
             if self.rect.bottom >= self.end_pos[1] and self.direction.y == 1:
                 self.direction.y = -1
@@ -61,8 +65,47 @@ class MovingSprite(Sprite):
             if self.rect.top <= self.start_pos[1] and self.direction.y == -1:
                 self.direction.y = 1
                 self.rect.top = self.start_pos[1]
+            self.reverse['y'] = True if self.direction.y > 0 else False
 
     def update(self, dt):
         self.old_rect = self.rect.copy()
         self.rect.topleft += self.direction * self.speed * dt
         self.check_border()
+
+        self.animate(dt)
+        if self.flip:
+            self.image = pygame.transform.flip(
+                self.image, self.reverse['x'], self.reverse['y'])
+
+
+class Spike(Sprite):
+
+    def __init__(self, pos, surf, groups, radius, speed, start_angle, end_angle, z=Z_LAYERS['main']):
+        self.center = pos
+        self.radius = radius
+        self.speed = speed
+        self.start_angle = start_angle
+        self.end_angle = end_angle
+        self.angle = self.start_angle
+        self.direction = 1
+        self.full_circle = True if self.end_angle == -1 else False
+
+        # trigonometry
+        y = self.center[1] + sin(radians(self.angle)) * self.radius
+        x = self.center[0] + cos(radians(self.angle)) * self.radius
+
+        super().__init__((x, y), surf, groups, z)
+
+    def update(self, dt):
+        self.angle += self.direction * self.speed * dt
+
+        if not self.full_circle:
+            if self.angle >= self.end_angle:
+                self.direction = -1
+            if self.angle < self.start_angle:
+                self.direction = 1
+
+        y = self.center[1] + sin(radians(self.angle)) * self.radius
+        x = self.center[0] + cos(radians(self.angle)) * self.radius
+
+        self.rect.center = (x, y)
