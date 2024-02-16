3f5181bf4612b6ef6aaa4f78372491e9a7ee3847
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-02-16 00:27:46 -0300

    feat: finished sky and background tiles

--- src/groups.py
+++ src/groups.py
@@ -1,23 +1,90 @@
 from settings import *
-from sprites import Sprite
+from sprites import Sprite, Cloud
+from random import choice, randint
+
 
 class AllSprites(pygame.sprite.Group):
-    def __init__(self, width, height, bg_tile=None):
+    def __init__(self, width, height, clouds, horizon_line, bg_tile=None, top_limit=0):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.offset = vector()
         self.width, self.height = width * TILE_SIZE, height * TILE_SIZE
+        self.borders = {
+            'left': 0,
+            'right': -self.width + WINDOW_WIDTH,
+            'bottom': -self.height + WINDOW_HEIGHT,
+            'top': top_limit,
+        }
+        self.sky = not bg_tile
+        self.horizon_line = horizon_line
 
         if bg_tile:
             for col in range(width):
-                for row in range(height):
+                for row in range(-int(top_limit / TILE_SIZE) - 1, height):
                     x, y = col * TILE_SIZE, row * TILE_SIZE
                     Sprite((x, y), bg_tile, self, -1)
+        else:
+            # ceu
+            self.large_cloud = clouds['large']
+            self.small_clouds = clouds['small']
+            self.cloud_direction = -1
 
-    def draw(self, target_pos):
+            # grandes nuvens
+            self.large_cloud_speed = 50
+            self.large_cloud_x = 0
+            self.large_cloud_tiles = int(
+                self.width / self.large_cloud.get_width()) + 2
+            self.large_cloud_width, self.large_cloud_height = self.large_cloud.get_size()
+
+            # pequenas nuvens
+            # cronômetro -> nuvem a cada 2,5 segundos
+            # muitas nuvens por padrão
+            for cloud in range(20):
+                pos = (randint(0, self.width), randint(self.borders['top'], self.horizon_line))
+                surf = choice(self.small_clouds)
+                Cloud(pos, surf, 1)
+
+    def camera_constraint(self):
+        self.offset.x = self.offset.x if self.offset.x < self.borders[
+            'left'] else self.borders['left']
+        self.offset.x = self.offset.x if self.offset.x > self.borders[
+            'right'] else self.borders['right']
+        self.offset.y = self.offset.y if self.offset.y > self.borders[
+            'bottom'] else self.borders['bottom']
+        self.offset.y = self.offset.y if self.offset.y < self.borders[
+            'top'] else self.borders['top']
+
+    def draw_sky(self):
+        self.display_surface.fill(SKY_COLOR)
+        horizon_pos = self.horizon_line + self.offset.y
+
+        # clouds
+        sea_rect = pygame.FRect(
+            0, horizon_pos, WINDOW_WIDTH, WINDOW_HEIGHT - horizon_pos)
+        pygame.draw.rect(self.display_surface, CLOUD_COLOR, sea_rect)
+
+        # horizon line
+        pygame.draw.line(self.display_surface, HORIZON_COLOR,
+                         (0, horizon_pos), (WINDOW_WIDTH, horizon_pos), 4)
+
+    def draw_large_cloud(self, dt):
+        self.large_cloud_x += self.cloud_direction * self.large_cloud_speed * dt
+        if self.large_cloud_x <= -self.large_cloud_width:
+            self.large_cloud_x = 0
+        for cloud in range(self.large_cloud_tiles):
+            left = self.large_cloud_x + self.large_cloud_width * cloud + self.offset.x
+            top = self.horizon_line - self.large_cloud_height + self.offset.y
+            self.display_surface.blit(self.large_cloud, (left, top))
+
+    def draw(self, target_pos, dt):
         self.offset.x = -(target_pos[0] - WINDOW_WIDTH / 2)
         self.offset.y = -(target_pos[1] - WINDOW_HEIGHT / 2)
+        self.camera_constraint()
 
-        for sprite in sorted(self, key = lambda sprite: sprite.z):
+        if self.sky:
+            self.draw_sky()
+            self.draw_large_cloud(dt)
+
+        for sprite in sorted(self, key=lambda sprite: sprite.z):
             offset_pos = sprite.rect.topleft + self.offset
             self.display_surface.blit(sprite.image, offset_pos)
--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,15 @@
 import pygame, sys
 from pygame.math import Vector2 as vector
 
+# settings
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 TILE_SIZE = 64
 ANIMATION_SPEED = 6
+
+# colors
+SKY_COLOR = '#DDC6A1'
+CLOUD_COLOR = '#92A9CE'
+HORIZON_COLOR = '#F5F1DE'
 
 # layers 
 Z_LAYERS = {
